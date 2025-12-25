5e5ec0060e9174f33699936553b87af1c02f52a3
hockiie <68018628+HOCKIIE@users.noreply.github.com>
2025-12-25 15:32:03 +0700

    refactor: standardize video file retrieval logic in videoEffect methods of IntroCtrl and SettingsCtrl

--- app/Http/Controllers/IntroCtrl.php
+++ app/Http/Controllers/IntroCtrl.php
@@ -9,25 +9,24 @@ class IntroCtrl extends Controller
 {
     public function videoEffect()
     {
-        try{
+        try {
             $prefix = '/storage/uploads/videos';
             $disk = Storage::disk(env('FILESYSTEM_DISK'));
-            
-            // หาไฟล์ intro_video.*
-            $file = collect($disk->files('videos'))
-                ->first(fn ($f) => str_starts_with(basename($f), 'intro_video.'));
 
-            $file = $disk->exists("$prefix/$file")
-                ? "$prefix/$file"
+            // หาไฟล์ intro_video.*
+            $file = collect($disk->files('uploads/videos'))
+                ->first(fn($f) => str_starts_with(basename($f), 'intro_video.'));
+
+            $file = $disk->exists("$file")
+                ? "/$file"
                 : "$prefix/default_video.mp4";
-            return response()->json($file,200);
-        } catch(\Exception $e) {
+            return response()->json($file, 200);
+        } catch (\Exception $e) {
             Log::error($e->getMessage());
             return response()->json([
                 'status' => false,
                 'message' => $e->getMessage(),
             ], 500);
         }
-
     }
 }
