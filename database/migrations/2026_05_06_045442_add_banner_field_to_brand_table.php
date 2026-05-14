afd3f26bde655522f380ccb6549ba1d5f6ac0885
HOCKIIIE <suphawat@ignite-idea.com>
2026-05-14 11:00:50 +0700

    Refactor: Add draftId field to Brand model migration

--- database/migrations/2026_05_06_045442_add_banner_field_to_brand_table.php
+++ database/migrations/2026_05_06_045442_add_banner_field_to_brand_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('brand', function (Blueprint $table) {
+            if (!Schema::hasColumn('brand', 'draftId')) $table->text('draftId')->nullable()->comment('Draft ID');
             if (!Schema::hasColumn('brand', 'banner')) $table->text('banner')->nullable()->comment('Banner Image');
             if (!Schema::hasColumn('brand', 'is_iframe')) $table->boolean('is_iframe')->default(false)->comment('Is Iframe');
         });
@@ -23,6 +24,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('brand', function (Blueprint $table) {
+            $table->dropColumn('draftId');
             $table->dropColumn('banner');
             $table->dropColumn('is_iframe');
         });
